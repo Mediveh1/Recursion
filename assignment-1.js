d90516d0d8cc6458dccc9e8df6f99ce67ec123dd
Hossam <tokyo.hossam@gmail.com>
2023-04-23 01:46:06 +0200

    done

--- assignment-1.js
+++ assignment-1.js
@@ -1,13 +1,26 @@
 //solving assignment 1 with iteration
-let arrFibs = [];
 
-function fibs(n) {
-  if (n < 2) {
-    return n;
+// function fibs(n) {
+//   if (n <= 2) {
+//     return [0, 1];
+//   } else {
+//     return [fibsR(num - 1), fibsR(num - 1)[num - 2] + fibsR(num - 1)[num - 3]];
+//   }
+// }
+
+// console.log(fibs(5));
+function fibsR(num) {
+  if (num <= 0) {
+    return "Must be greater than 0";
+  } else if (num <= 1) {
+    return [0];
+  } else if (num <= 2) {
+    return [0, 1];
   } else {
-    arrFibs.push(fibs(n - 1) + fibs(n - 2));
-    return fibs(n - 1) + fibs(n - 2);
+    return [
+      ...fibsR(num - 1),
+      fibsR(num - 1)[num - 2] + fibsR(num - 1)[num - 3],
+    ];
   }
 }
-console.log(fibs(5));
-console.log(arrFibs);
+console.log(fibsR(8));
