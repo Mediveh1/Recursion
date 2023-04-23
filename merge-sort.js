09bb3d8e7be5f3445e5fbb50e80dc6362e48be9a
Hossam <tokyo.hossam@gmail.com>
2023-04-23 03:47:13 +0200

    added merge sort assignment

---
+++ merge-sort.js
@@ -0,0 +1,26 @@
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  let mid = Math.floor(array.length / 2);
+  //left array
+  let left = mergeSort(array.slice(0, mid));
+  let right = mergeSort(array.slice(mid));
+  return merge(mergeSort(left), mergeSort(right));
+}
+function merge(leftArray, rightArray) {
+  const tempArray = [];
+
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    if (leftArray[0] <= rightArray[0]) {
+      tempArray.push(leftArray.shift());
+    } else {
+      tempArray.push(rightArray.shift());
+    }
+  }
+  return tempArray.concat(leftArray, rightArray);
+}
+let arr = [5, 4, 1];
+
+console.log(mergeSort(arr));
